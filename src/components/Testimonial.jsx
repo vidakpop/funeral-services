5b94ef86bd0df5e36198fac3ff7b35c197b2bcc6
vidakpop <daviskpop@gmail.com>
2024-12-22 19:03:45 +0300

    Add testimonials data to Testimonial component

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const Testimonial = () => {
+    const testimonials = [
+        { name: "John Doe", message: "They made everything so easy during our hard time." },
+        { name: "Jane Smith", message: "Exceptional service and care." },
+        { name: "Robert K.", message: "Highly recommended!" },
+      ];
   return (
     <div>Testimonial</div>
   )
