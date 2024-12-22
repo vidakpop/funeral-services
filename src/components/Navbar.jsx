0dde932cb63e18a37ded2e01e228ef4e415f7c79
vidakpop <daviskpop@gmail.com>
2024-12-22 11:31:42 +0300

    `Refactor App component and update tailwind config`

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar =() => {
+    return (
+        <nav className="bg-gradient-to-r from-blue-800 to-purple-800 shadow-lg py-4">
+            <div className="container mx-auto flex justify-between items-center px-4">
+                <h1 className="text-white text-2xl font-bold">KephaCare Services</h1>
+                <ul >
+
+                </ul>
+
+            </div>
+        </nav>
+    )
+}
