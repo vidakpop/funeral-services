23b595002ee3ed64f39c3b24307b953e838466d5
vidakpop <daviskpop@gmail.com>
2024-12-22 18:40:08 +0300

    Add Pricing component with service pricing details

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const pricing=[
+    { service: "Transport", price: "KES 10,000" },
+    { service: "Casket", price: "KES 50,000" },
+    { service: "Lowering Gear", price: "KES 15,000" },
+    { service: "Gazebo", price: "KES 20,000" },
+    { service: "Red/Green Carpet", price: "KES 5,000" },
+]
+const Pricing = () => {
+  return (
+    <div>Pricing</div>
+  )
+}
+
+export default Pricing
