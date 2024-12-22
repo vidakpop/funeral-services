f2b77eadca913109388945f4bb167e54025f1043
vidakpop <daviskpop@gmail.com>
2024-12-22 19:26:08 +0300

    Add dynamic step display to Workflow component

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -11,6 +11,17 @@ const Workflow = () => {
   return (
     <section id='workflow' className='py-16 bg-gradient-to-r from-blue-50 to-purple-50'>
        <h2 className='text-center text-3xl font-bold text-gray-900 mb-10'>How it works</h2>  
+        <div className='container mx-auto flex flex-col items-cemter space-y-6 px-4'>
+            {steps.map((step, index)=>(
+                <div
+                key={index}
+                className="bg-white text-gray-900 p-6 shadow-lg rounded-lg w-full md:w-3/4 text-center"
+              >
+                <p className="text-lg font-semibold">{index + 1}. {step}</p>
+              </div>
+            ))}
+
+        </div>
 
     </section>
   )
