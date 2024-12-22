59a38844a36cbf4fafaa1340fae5da0ad0d6b342
vidakpop <daviskpop@gmail.com>
2024-12-22 12:15:38 +0300

    Add heading and description to HeroSection component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,6 +11,8 @@ const HeroSection = () => {
         </video>
         <div className='absolute inset-0 bg-black bg-opacity-50'></div>
         <div className='relative z-10 flex flex-col items-center justify-center h-full text-white text-center'>
+           <h1 className='text-4xl md:text-6xl font-extrabold mb-4'>Providing Comfort During Different Times</h1>
+           <p className='text-lg md:text-xl mb-6'>Premium services are here to ease the burden during your time of need.</p>
            
         </div>     
     </div>
