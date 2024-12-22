e6a07d50c9bedce647d90131512e5eb2a5b774c0
vidakpop <daviskpop@gmail.com>
2024-12-22 11:51:03 +0300

    Add HeroSection component with initial structure

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='relative h-screen'
+        <video className='absolute inset-0'
+
+    </div>
+  )
+}
+
+export default HeroSection
