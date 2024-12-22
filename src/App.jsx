ca2c02b8d69b37671cdd07907cd08bf538282349
vidakpop <daviskpop@gmail.com>
2024-12-22 12:35:08 +0300

    Add FeatureSection component to App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
 
 function App () {
   return (
     <div className='bg-gray-50 text-gray-900'>
       <Navbar />
       <HeroSection />
+      <FeatureSection />
     </div>
   )
 }
