5fda25e0be825d1a4eec1d986640de5047461cc9
vidakpop <daviskpop@gmail.com>
2024-12-22 12:04:42 +0300

    Integrate HeroSection component into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 function App () {
   return (
     <div className='bg-gray-50 text-gray-900'>
       <Navbar />
+      <HeroSection
     </div>
   )
 }
