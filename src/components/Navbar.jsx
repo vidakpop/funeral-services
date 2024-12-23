5d32430c8522a536de848a21025e7c95da2e45b9
vidakpop <daviskpop@gmail.com>
2024-12-23 16:58:24 +0300

    Refactor Navbar component styles for improved aesthetics and responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 
-const Navbar =() => {
+const Navbar = () => {
     return (
-        <nav className="bg-gradient-to-r from-blue-800 to-purple-800 shadow-lg py-4">
+        <nav className="bg-black sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80 ">
             <div className="container mx-auto flex justify-between items-center px-4">
-                <h1 className="text-white text-2xl font-bold">KephaCare Services</h1>
+                <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-yellow-600 text-2xl font-bold">KephaCare Services</h1>
                 <ul className="flex space-x-6">
-                    <li><a href="#services" className="text-white hover:text-yellow-300">Services</a></li>
-                    <li><a href="#pricing" className="text-white hover:text-yellow-300">Pricing</a></li>
-                    <li><a href="#testimonials" className="text-white hover:text-yellow-300">Testimonials</a></li>
-                    <li><a href="#contact" className="text-white hover:text-yellow-300">Contact</a></li>
-
+                    <li><a href="#services" className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-yellow-600 hover:text-yellow-300">Services</a></li>
+                    <li><a href="#pricing" className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-yellow-600 hover:text-yellow-300">Pricing</a></li>
+                    <li><a href="#testimonials" className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-yellow-600 hover:text-yellow-300">Testimonials</a></li>
+                    <li><a href="#contact" className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-yellow-600 hover:text-yellow-300">Contact</a></li>
                 </ul>
-
             </div>
         </nav>
-    )
+    );
 }
+
 export default Navbar;
