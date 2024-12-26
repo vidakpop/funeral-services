3e3c7c43febefe6cf28ea43548c2dea1cbc74104
vidakpop <daviskpop@gmail.com>
2024-12-26 11:40:02 +0300

    Enhance HeroSection heading with gradient styling for improved visual impact

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,7 +13,9 @@ const HeroSection = () => {
         </video>
         <div className='absolute inset-0 bg-black bg-opacity-50'></div>
         <div className='relative z-10 flex flex-col items-center justify-center h-full text-white text-center'>
-           <h1 className='text-4xl md:text-6xl font-extrabold mb-4'>Providing Comfort During Difficult Times</h1>
+           <h1 className='text-4xl md:text-6xl font-extrabold mb-4'>Providing Comfort During
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-yellow-600 hover:text-yellow-300'> Difficult Times</span>
+           </h1>
            <p className='text-lg md:text-xl mb-6'>Premium services are here to ease the burden during your time of need.</p>
            <button className='bg-gradient-to-r from-yellow-400 to-yellow-600 text-black px-6 py-3 rounded-full font-semibold hover:bg-yellow-500 transition'>Book Our Services</button>
         </div>     
