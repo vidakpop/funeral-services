b27f03078547b134cf698181bc83a8e40e0dccba
vidakpop <daviskpop@gmail.com>
2024-12-26 12:56:30 +0300

    remove popup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import FeatureSection from './components/FeatureSection'
 import Pricing from './components/Pricing'
 import Testimonial from './components/Testimonial'
 import Workflow from './components/Workflow'
-import EncouragementPopup from './components/EncouragementPopup'
+
 import Location from './components/Location'
 import Footer from './components/Footer'
 
@@ -18,7 +18,6 @@ function App () {
       <Pricing/>
       <Workflow />
       <Testimonial/>
-     /* <EncouragementPopup/>*/
       <Location />
       <Footer />
     </div>
