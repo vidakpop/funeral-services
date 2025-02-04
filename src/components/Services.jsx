42d79250d6990c17d9874eb2ea0245ceb17672b3
vidakpop <daviskpop@gmail.com>
2025-02-04 12:56:30 +0300

    Fetch services data from API in Services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,8 +4,14 @@ const Services = () => {
     const [services, setServices] = useState([])
     const API_URL="http://127.0.0.1:/api" //update during production
     useEffect(()=>{
-        
-    })
+        axios.get(API_URL)
+            .then(response =>{
+                setServices(response.data)
+            })
+            .catch(error => {
+                console.error('Error fetching services:',error)
+            })
+    },[])
   return (
     <div>Services</div>
   )
