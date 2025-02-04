c19dc915ebe1e65bfb2e8cc48a2c01c9f334bb01
vidakpop <daviskpop@gmail.com>
2025-02-04 12:48:29 +0300

    Implement useEffect hook in Services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,9 @@ import React,{useEffect,useState} from 'react'
 const Services = () => {
     const [services, setServices] = useState([])
     const API_URL="http://127.0.0.1:/api" //update during production
-    useEffe
+    useEffect(()=>{
+        
+    })
   return (
     <div>Services</div>
   )
