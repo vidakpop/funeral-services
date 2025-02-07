cbd25aeca90912729d9e11f409b7bf22a21510a3
vidakpop <daviskpop@gmail.com>
2025-02-07 12:29:09 +0300

    Remove Services component from App layout

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App () {
       <Navbar />
       <HeroSection />
       <FeatureSection />
-      <Services/>
+     {/* <Services /> */}
       <Pricing/>
       <Workflow />
       <Testimonial/>
